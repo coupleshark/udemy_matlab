7ab3710e32399a704d97012520711317d4381248
ansha <ansha@opt.cn>
2021-07-28 23:47:55 +0800

    add startup.m

---
+++ scripts/startup.m
@@ -0,0 +1,7 @@
+cd 'D:\study\matlab\matlab learning\udemy\udemy_matlab'
+clc, disp('Work hard and have fun.')
+beep off
+addpath('D:\study\matlab\matlab learning\udemy\udemy_matlab')
+
+set(0, 'DefaultFigureWindowStyle','docked')
+set(0, 'DefaultFigureColormap', jet), close all
